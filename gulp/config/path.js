1693b485383fcc4afc147d3da64cf6f7d4729180
Dmitrii Chizhov <dimachig@gmail.com>
2024-01-10 12:08:13 +0300

    add: Dist and Src paths

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,4 +1,20 @@
-
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
 
 const buildFolder = `./dist`;
 const srcFolder = `./src`;
+
+export const path = {
+  build: {
+    files: `${buildFolder}/files/`,
+  },
+  src: {
+    files: `${srcFolder}/files/**/*.*`, //Абсолютно все файлы и папки
+  },
+  watch: {},
+  clean: buildFolder,
+  buildFolder: buildFolder,
+  srcFolder: srcFolder,
+  rootFolder: rootFolder,
+  ftp: ''
+}
