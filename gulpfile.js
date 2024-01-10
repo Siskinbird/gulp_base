38ff4c52690d0dec2edad570c562845715782ea2
Dmitrii Chizhov <dimachig@gmail.com>
2024-01-10 16:14:34 +0300

    feat: scss, autoprefix, webp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,37 @@
+/** Основной модуль Gulp */
 import gulp from 'gulp';
+
+/** Импорт путей */
 import { path } from './gulp/config/path.js';
 
+/** Импорт общих плагинов */
+import { plugins } from './gulp/config/plugins.js';
 
-//Передаём значение в глобальную переменную
+
+/** Передаём значение в глобальную переменную*/
 global.app = {
   path: path,
-  gulp: gulp
+  gulp: gulp,
+  plugins: plugins
 }
 
 //Импорт задач
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
+import { server } from './gulp/tasks/server.js';
+import {scss } from './gulp/tasks/scss.js';
 
 
 const watcher = () => {
   gulp.watch(path.watch.files, copy)
   gulp.watch(path.watch.html, html)
+  gulp.watch(path.watch.scss, scss)
 }
 
-const mainTasks = gulp.parallel(copy, html)
+const mainTasks = gulp.parallel(copy, html, scss)
 
-const dev = gulp.series(reset, mainTasks, watcher)
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 
 //Выполнение сценария по умолчанию 
 gulp.task('default', dev);
