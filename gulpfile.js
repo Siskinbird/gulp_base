16e1a101f3d155a0dad859bd434072fd60dcf205
Dmitrii Chizhov <dimachig@gmail.com>
2024-01-10 13:52:15 +0300

    feat: include plagin

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,17 @@ global.app = {
 //Импорт задач
 import { copy } from './gulp/tasks/copy.js';
 import { reset } from './gulp/tasks/reset.js';
+import { html } from './gulp/tasks/html.js';
 
 
 const watcher = () => {
   gulp.watch(path.watch.files, copy)
+  gulp.watch(path.watch.html, html)
 }
 
-const dev = gulp.series(reset, copy, watcher)
+const mainTasks = gulp.parallel(copy, html)
+
+const dev = gulp.series(reset, mainTasks, watcher)
 
 //Выполнение сценария по умолчанию 
 gulp.task('default', dev);
