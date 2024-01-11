e5f817699019f1296beb049fc44274835cf2368d
Dmitrii Chizhov <dimachig@gmail.com>
2024-01-11 15:42:39 +0300

    bug: cant create fonts folder in dist folder

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import fonter from 'gulp-fonter';
+import fonter from 'gulp-fonter-fix';
 import ttf2woff2 from 'gulp-ttf2woff2';
 
 export const otfToTtf = () => {
@@ -31,8 +31,11 @@ export const ttfToWoff = () => {
   .pipe(app.gulp.dest(`${app.path.build.fonts}`))
 
   .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.ttf`))
+  .pipe(app.gulp.src(`${app.path.srcFolder}/fonts/*.woff`))
+  //.pipe(app.gulp.dest(`${app.path.build.fonts}`))
 
   .pipe(ttf2woff2())
+
   .pipe(app.gulp.dest(`${app.path.build.fonts}`))
 }
 
@@ -42,7 +45,7 @@ export const fontsStyle = () => {
   fs.readdir(app.path.build.fonts, function (err, fontsFiles) {
     if(fontsFiles) {
       if(!fs.existsSync(fontsFile)) {
-        fs.wwriteFile(fontsFile, '', cb);
+        fs.writeFile(fontsFile, '', cb);
         let newFileOnly;
         for(let i = 0; i < fontsFiles.length; i++) {
           let fontFileName = fontsFiles[i].split('.')[0]
@@ -68,14 +71,7 @@ export const fontsStyle = () => {
             } else {
               fontWeight = 400;
             }
-            fs.appendFile(fontsFile,
-              `@font-face {
-                font-family: ${fontName};
-                font-display: swap;
-                src: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") 
-                font-weight: ${fontWeight};
-                font-style: normal;
-              }\r\n`, cb);
+            fs.appendFile(fontsFile, `@font-face{\n\tfont-family: ${fontName};\n\tfont-display: swap;\n\tsrc: url("../fonts/${fontFileName}.woff2") format("woff2"), url("../fonts/${fontFileName}.woff") format("woff");\n\tfont-weight: ${fontWeight};\n\tfont-style: normal;\n}\r\n`, cb);
               newFileOnly = fontFileName
             }
           }
